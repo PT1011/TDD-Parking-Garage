04283646280f9fedf4d32929f8edff9c3b6a1d21
Parth Thite <parthpthite@gmail.com>
2026-04-23 07:53:41 -0400

    fixed syntax error

--- test_garage.py
+++ test_garage.py
@@ -1,4 +1,4 @@
 from garage import enter_garage
 
 def test_enter_garage_works()
-    assert enter_garage({'Capacity: 10', 'cars': {'Bugatti17': 13}})
+    assert enter_garage({'Capacity': 10, 'cars': {'Bugatti17': 13}})
