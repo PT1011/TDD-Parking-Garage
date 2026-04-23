2fb5cb20fa3abdac94a3ad43f3d8bbf94312585d
Parth Thite <parthpthite@gmail.com>
2026-04-23 08:30:09 -0400

    create first test for exit garage, fails

--- test_garage.py
+++ test_garage.py
@@ -32,3 +32,11 @@ def test_enter_garage_car_time_not_int():
         "cars": {}         # car_id -> entry_hour (int)
         }
         enter_garage(garage_dict, 'Bugatti12', 'fourteen')
+
+def test_exit_garage_works():
+    garage_dict = {
+        "capacity": 10,   # total number of spots
+        "cars": {'Toyota': 14}         # car_id -> entry_hour (int)
+        }
+    exit_garage(garage_dict, 'Toyota')
+    assert 'Toyota' garage_dict['cars'].keys()
