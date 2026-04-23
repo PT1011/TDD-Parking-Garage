b4ee712cd2f0edaf01cfa829f4e8523a886bf2f5
Parth Thite <parthpthite@gmail.com>
2026-04-23 07:59:57 -0400

    fixed enter_garage test, test fails

--- test_garage.py
+++ test_garage.py
@@ -1,4 +1,10 @@
 from garage import enter_garage
 
 def test_enter_garage_works():
-    assert enter_garage({'Capacity': 10, 'cars': {'Bugatti17': 13}})
+    garage_dict = {
+    "capacity": 10,   # total number of spots
+    "cars": {}         # car_id -> entry_hour (int)
+    }
+
+    enter_garage(garage_dict, 'Bugatti17', 13)
+    assert 'Bugatti17' in garage_dict[cars].keys()
