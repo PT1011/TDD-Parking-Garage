dafc93f7749f98752c4667937e00ba1960817457
Parth Thite <parthpthite@gmail.com>
2026-04-24 07:38:15 -0400

    deleted previous test as parametrize tests it also

--- test_garage.py
+++ test_garage.py
@@ -65,8 +65,6 @@ def test_get_available_spots_garage_is_full():
     
     assert get_available_spots(garage_dict) == 0
 
-def test_calculate_fee_works():
-    assert calculate_fee(3, 2) == 6.00
 
 @pytest.mark.parametrize("hours, rate, expected",[(3, 2, 6.00), (11, 2, 22.00), (10, 0.2, 2.00)])
 def test_calculate_fee_multiple_test(hours, rate, expected):
